26bf0231194d6ecfabbb8ac90d9f7fc29827ada0
Prima Wirawan <primailkom@gmail.com>
2020-05-11 23:13:54 +0700

    Solve including static files

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ router.get('/',function(req,res){
 //   res.sendFile(path.join(__dirname+'/sitemap.html'));
 // });
 
+// serve static files
+app.use(express.static('.'))
+
 //add the router
 app.use('/', router);
 app.listen(process.env.port || 3000);
